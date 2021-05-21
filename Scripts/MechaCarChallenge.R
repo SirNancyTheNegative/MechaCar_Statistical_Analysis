d7b45342b10c8fc177bab3e53049f21e08a16512
SirNancyTheNegative <77301673+SirNancyTheNegative@users.noreply.github.com>
2021-05-21 16:59:35 -0700

    T-Test code reworked

--- Scripts/MechaCarChallenge.R
+++ Scripts/MechaCarChallenge.R
@@ -1,5 +1,4 @@
 library(dplyr)
-library(tidyverse)
 
 mechacars <- read_csv("../Resources/MechaCar_mpg.csv")
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacars))
@@ -8,3 +7,8 @@ suspensioncoil <- read_csv("../Resources/Suspension_Coil.csv")
 
 total_summary <- suspensioncoil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- suspensioncoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+t.test(suspensioncoil$PSI, mu=1500)
+t.test(x = (suspensioncoil %>% filter(Manufacturing_Lot == "Lot1"))$PSI, mu=1500)
+t.test(x = (suspensioncoil %>% filter(Manufacturing_Lot == "Lot2"))$PSI, mu=1500)
+t.test(x = (suspensioncoil %>% filter(Manufacturing_Lot == "Lot3"))$PSI, mu=1500)
